fe72139c510e1a47b9d16393d92242993d4d2792
Adam Stohl <iohzrd@gmail.com>
2020-07-25 18:40:32 -0700

    orbit test

--- orbitTest1.js
+++ orbitTest1.js
@@ -1,39 +1,47 @@
 // const fs = require("fs-extra")
-const IpfsHttpClient = require('ipfs-http-client')
-const ipfs = IpfsHttpClient()
-const Orbit = require('orbit_')
+const IpfsHttpClient = require("ipfs-http-client");
+const ipfs = IpfsHttpClient();
+const Orbit = require("orbit_");
+
+function test(arg) {
+  const self = Boolean(arg);
+  console.log(self);
+}
 
 async function main() {
-    try {
-        const { id } = await ipfs.id()
-        console.log("id")
-        console.log(id)
-        const orbit = new Orbit(ipfs)
+  try {
+    const { id } = await ipfs.id();
+    console.log("id");
+    console.log(id);
 
-        const username = id
-        const channel = 'QmUsHMBjzxGMJy19d9XUNHKRQ21eUNsRAdAa8VMrJbQowJ_comments'
+    const orbit = new Orbit(ipfs);
+    console.log(orbit._options);
 
-        orbit.events.on('connected', () => {
-            console.log(`-!- Orbit connected`)
-            orbit.join(channel)
-        })
+    const channel = "chat";
 
-        orbit.events.on('joined', channelName => {
-            orbit.send(channelName, `${id} is now caching this channel`)
-            console.log(`-!- Joined # ${channelName}`)
-        })
+    orbit.events.on("connected", () => {
+      console.log(`-!- Orbit connected`);
+      orbit.join(channel);
+    });
 
-        // Listen for new messages
-        orbit.events.on('entry', (entry, channelName) => {
-            const post = entry.payload.value
-            console.log(`${post.meta.ts} - ${post.meta.from.name} - ${post.content} - ${channelName}`)
-        })
+    orbit.events.on("joined", channelName => {
+      orbit.send(channelName, `is now caching this channel`);
+      console.log(`-!- Joined # ${channelName}`);
+    });
 
-        // Connect to Orbit network
-        orbit.connect(username).catch(e => console.error(e))
-    } catch (error) {
-        console.log(error)
-    }
+    // Listen for new messages
+    orbit.events.on("entry", (entry, channelName) => {
+      const post = entry.payload.value;
+      console.log("post");
+      console.log(post);
+      console.log(channelName);
+    });
 
+    // Connect to Orbit network
+    orbit.connect(id).catch(e => console.error(e));
+  } catch (error) {
+    console.log(error);
+  }
+  test();
 }
-main()
+main();
