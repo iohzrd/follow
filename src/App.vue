0d72e7f8ef018045b7467acfe24f124deacbe11e
Adam Stohl <iohzrd@gmail.com>
2020-12-17 22:08:32 -0800

    fix publish timer

--- src/App.vue
+++ src/App.vue
@@ -126,18 +126,18 @@ const menuList = [
   {
     icon: "rss_feed",
     label: "Feed",
-    route: "Feed"
+    route: "Feed",
   },
   {
     icon: "assignment_ind",
     label: "Profile",
-    route: "Identity"
+    route: "Identity",
   },
   {
     icon: "settings",
     label: "Settings",
-    route: "Settings"
-  }
+    route: "Settings",
+  },
 ];
 
 export default {
@@ -155,41 +155,41 @@ export default {
       refreshInterval: null,
       unfollowPrompt: false,
       shareLinkPrompt: false,
-      shareLink: ""
+      shareLink: "",
     };
   },
 
   watch: {
     dark: {
-      handler: function(after) {
+      handler: function (after) {
         this.dark = after;
         this.$q.dark.set(this.dark);
-      }
-    }
+      },
+    },
   },
   created() {
     this.$q.dark.set(true);
   },
-  beforeDestroy: function() {
+  beforeDestroy: function () {
     clearInterval(this.publishInterval);
     clearInterval(this.refreshInterval);
   },
-  mounted: function() {
-    ipcRenderer.invoke("get-id", this.id).then(id => {
+  mounted: function () {
+    ipcRenderer.invoke("get-id", this.id).then((id) => {
       console.log("getIdentity.then");
       console.log(id);
       this.ipfs_id = id;
       this.$store.commit("setID", id);
     });
 
-    ipcRenderer.send("publish");
+    ipcRenderer.send("publish-identity");
     ipcRenderer.send("update-following");
 
-    this.publishInterval = setInterval(async function() {
+    this.publishInterval = setInterval(async function () {
       console.log("auto-publish...");
-      ipcRenderer.send("publish");
-    }, 60 * 60 * 1000);
-    this.refreshInterval = setInterval(async function() {
+      ipcRenderer.send("publish-identity");
+    }, 10 * 60 * 1000);
+    this.refreshInterval = setInterval(async function () {
       console.log("refreshing identities...");
       ipcRenderer.send("update-following");
     }, 1 * 60 * 1000);
@@ -212,8 +212,8 @@ export default {
     async removeFollowing() {
       ipcRenderer.send("unfollow", this.idToUnollow);
       this.idToUnollow = "";
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
